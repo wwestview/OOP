abdf2eeb83cba27898dac32fe4527e19576ddd4d
Nazar Kolachko <biokapt@gmail.com>
2024-11-03 20:07:29 +0200

    Add Sum1 and Sum2

--- MyFraction/FractionOperation.cs
+++ MyFraction/FractionOperation.cs
@@ -20,6 +20,23 @@
     {
         return new MyFrac(f1.nom * f2.denom, f1.denom * f2.nom);
     }
-
+    public static MyFrac CalcSum(int n)
+    {
+        MyFrac fr = new(0, 1);
+        for (long i = 1; i <= n; i++)
+        {
+            fr = fr + new MyFrac(1, i * (i + 1));
+        }
+        return fr;
+    }
+    public static MyFrac CalcSum2(int n)
+    {
+        MyFrac fr = new(1, 1);
+        for (long i = 1; i <= n; i++)
+        {
+            fr = fr * (new MyFrac(1,1)- new MyFrac(1,i*i));
+        }
+        return fr;
+    }
 
 }
